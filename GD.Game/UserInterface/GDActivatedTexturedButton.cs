af15665dbaa60ec10c6d0ec8fec46cfa9c42e538
Andrei Zavatski <megaman9919@gmail.com>
2021-04-22 11:58:44 +0300

    Implement GDActivatedTexturedButton component

---
+++ GD.Game/UserInterface/GDActivatedTexturedButton.cs
@@ -0,0 +1,24 @@
+﻿using GD.Game.Graphics;
+using osu.Framework.Bindables;
+using osu.Framework.Graphics;
+using osuTK.Graphics;
+
+namespace GD.Game.UserInterface
+{
+    public class GDActivatedTexturedButton : GDTexturedButton
+    {
+        public readonly BindableBool IsActive = new();
+
+        public GDActivatedTexturedButton(string textureName, FlipOrientation flipOrientation = FlipOrientation.None, float baseScale = 0.84f, bool useLarge = false)
+            : base(textureName, flipOrientation, baseScale, useLarge)
+        {
+            ClickAction = IsActive.Toggle;
+        }
+
+        protected override void LoadComplete()
+        {
+            base.LoadComplete();
+            IsActive.BindValueChanged(active => this.FadeColour(active.NewValue ? Color4.White : Color4.Gray), true);
+        }
+    }
+}
---
+++ GD.Tests/Visual/TestSceneGDActivatedTexturedButton.cs
@@ -0,0 +1,18 @@
+﻿using GD.Game.UserInterface;
+using osu.Framework.Testing;
+using osu.Framework.Graphics;
+
+namespace GD.Tests.Visual
+{
+    public class TestSceneGDActivatedTexturedButton : TestScene
+    {
+        public TestSceneGDActivatedTexturedButton()
+        {
+            Add(new GDActivatedTexturedButton("diff-legacy/Demon")
+            {
+                Anchor = Anchor.Centre,
+                Origin = Anchor.Centre
+            });
+        }
+    }
+}
